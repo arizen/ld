4077ec81ca876a060ebb313e9a67b03a522b117b
estaril <ceironwars@gmail.com>
2014-06-12 00:53:49 +0300

    6

--- app/views/logout.blade.php
+++ app/views/logout.blade.php
@@ -10,5 +10,7 @@
 
          <div align="center" class="alert alert-success">
     <strong style="font-size:30px;">Başarıyla Çıkış Yaptınız!</strong></div>
+    <meta http-equiv="refresh" content="3;URL=http://leagueduo.com">
+
 
 @stop
