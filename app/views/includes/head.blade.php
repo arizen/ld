ff91f8f2114015597ab0c2262e645e31a551a300
estaril <ceironwars@gmail.com>
2014-06-11 19:32:36 +0300

    allaalla

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -34,6 +34,36 @@
         
 
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false"></script>
+        <script type="text/javascript" src="js/_jq.js"></script>
+<script type="text/javascript" src="js/_jquery.placeholder.js"></script>
+<script src="js/activeaxon_menu.js" type="text/javascript"></script> 
+<script src="js/animationEnigne.js" type="text/javascript"></script> 
+<script src="js/bootstrap.js" type="text/javascript"></script> 
+<script src="js/bootstrap.min.js" type="text/javascript"></script> 
+<script src="js/easypiecharts.js" type="text/javascript"></script> 
+<script src="js/ie-fixes.js" type="text/javascript"></script> 
+<script src="js/jquery.base64.js" type="text/javascript"></script> 
+<script src="js/jquery.carouFredSel-6.2.1-packed.js" type="text/javascript"></script> 
+<script src="js/jquery.cycle.js" type="text/javascript"></script> 
+<script src="js/jquery.cycle2.carousel.js" type="text/javascript"></script> 
+<script src="js/jquery.easing.1.3.js" type="text/javascript"></script> 
+<script src="js/jquery.easytabs.js" type="text/javascript"></script> 
+<script src="js/jquery.eislideshow.js" type="text/javascript"></script> 
+<script src="js/jquery.flexslider.js" type="text/javascript"></script> 
+<script src="js/jquery.infinitescroll.js" type="text/javascript"></script> 
+<script src="js/jquery.isotope.js" type="text/javascript"></script> 
+<script src="js/jquery.parallax-1.1.3.js" type="text/javascript"></script> 
+<script src="js/jquery.prettyPhoto.js" type="text/javascript"></script> 
+<script src="js/jQuery.scrollPoint.js" type="text/javascript"></script> 
+<script src="js/jquery.themepunch.plugins.min.js" type="text/javascript"></script> 
+<script src="js/jquery.themepunch.revolution.js" type="text/javascript"></script> 
+<script src="js/jquery.tipsy.js" type="text/javascript"></script> 
+<script src="js/jquery.validate.js" type="text/javascript"></script> 
+<script src="js/jQuery.XDomainRequest.js" type="text/javascript"></script> 
+<script src="js/retina.js" type="text/javascript"></script> 
+<script src="js/timeago.js" type="text/javascript"></script> 
+<script src="js/tweetable.jquery.js" type="text/javascript"></script> 
+<script src="js/zeina.js" type="text/javascript"></script> 
 
         <!-- Fonts -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -55,36 +55,7 @@
 </footer>
 
 </div><!-- wrapper end -->
-<script type="text/javascript" src="js/_jq.js"></script>
-<script type="text/javascript" src="js/_jquery.placeholder.js"></script>
-<script src="js/activeaxon_menu.js" type="text/javascript"></script> 
-<script src="js/animationEnigne.js" type="text/javascript"></script> 
-<script src="js/bootstrap.js" type="text/javascript"></script> 
-<script src="js/bootstrap.min.js" type="text/javascript"></script> 
-<script src="js/easypiecharts.js" type="text/javascript"></script> 
-<script src="js/ie-fixes.js" type="text/javascript"></script> 
-<script src="js/jquery.base64.js" type="text/javascript"></script> 
-<script src="js/jquery.carouFredSel-6.2.1-packed.js" type="text/javascript"></script> 
-<script src="js/jquery.cycle.js" type="text/javascript"></script> 
-<script src="js/jquery.cycle2.carousel.js" type="text/javascript"></script> 
-<script src="js/jquery.easing.1.3.js" type="text/javascript"></script> 
-<script src="js/jquery.easytabs.js" type="text/javascript"></script> 
-<script src="js/jquery.eislideshow.js" type="text/javascript"></script> 
-<script src="js/jquery.flexslider.js" type="text/javascript"></script> 
-<script src="js/jquery.infinitescroll.js" type="text/javascript"></script> 
-<script src="js/jquery.isotope.js" type="text/javascript"></script> 
-<script src="js/jquery.parallax-1.1.3.js" type="text/javascript"></script> 
-<script src="js/jquery.prettyPhoto.js" type="text/javascript"></script> 
-<script src="js/jQuery.scrollPoint.js" type="text/javascript"></script> 
-<script src="js/jquery.themepunch.plugins.min.js" type="text/javascript"></script> 
-<script src="js/jquery.themepunch.revolution.js" type="text/javascript"></script> 
-<script src="js/jquery.tipsy.js" type="text/javascript"></script> 
-<script src="js/jquery.validate.js" type="text/javascript"></script> 
-<script src="js/jQuery.XDomainRequest.js" type="text/javascript"></script> 
-<script src="js/retina.js" type="text/javascript"></script> 
-<script src="js/timeago.js" type="text/javascript"></script> 
-<script src="js/tweetable.jquery.js" type="text/javascript"></script> 
-<script src="js/zeina.js" type="text/javascript"></script> 
+
     
 </body>
 </html>
