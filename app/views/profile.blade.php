c8933763df16968b48e685c490820258e1181c81
estaril <ceironwars@gmail.com>
2014-06-12 22:57:41 +0300

    asd

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -8,7 +8,7 @@
                 <div class="callout-box clearfix">  
 
 
-                    <div class="col-md-3 col-sm-6 col-xs-10"><b style="margin-left:none; color:red; font-size:28px;">yuser</b><br /><br /><img src="http://avatar-user.s.aeriastatic.com/61386851/0327ddc4-84fe-4306-a367-80a8e947171e"></div> 
+                    <div class="col-md-3 col-sm-6 col-xs-10"><b style="margin-left:none; color:red; font-size:28px;">{{ $user->username }}</b><br /><br /><img src="http://avatar-user.s.aeriastatic.com/61386851/0327ddc4-84fe-4306-a367-80a8e947171e"></div> 
                     
 
                     <div class="col-md-3 col-sm-6 col-xs-10"><div class="col-md-12 col-sm-6 col-xs-4"><b style="color:#2772db; font-size:20px;">Güncel Ligim:</b><p style="color:#f7b53b;">ALTIN III</p></div><div class="col-md-12 col-sm-6 col-xs-4"><b style="color:#2772db; font-size:20px;"><img src="/images/ldicon.png" width="64" height="64"> Puanım :</b><p style="color:red;">200 LDP</p></div><div class="col-md-12 col-sm-6 col-xs-4"><b style="color:#2772db; font-size:20px;">Son Maçım:</b><p style="color:green;">Galibiyet</p></div></div> 
