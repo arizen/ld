8a7f723d849778af86552147d7097b94cbcd6e66
estaril <ceironwars@gmail.com>
2014-06-12 17:54:55 +0300

    12312

--- app/views/step3.blade.php
+++ app/views/step3.blade.php
@@ -2,8 +2,9 @@
 @section('content')
 
 {{ HTML::ul($errors->all()) }}
-<div class="alert alert-warning" style="float:center;"><strong>İLETİŞİM BİLGİSİ VERMEK ZORUNLU DEĞİLDİR TAMAMLA! DİYEREK GEÇEBİLİRSİNİZ.</strong></div>
+
 <center>
+<div class="alert alert-warning"><strong>İLETİŞİM BİLGİSİ VERMEK ZORUNLU DEĞİLDİR "TAMAMLA!" DİYEREK GEÇEBİLİRSİNİZ.</strong></div>
 {{ Form::open(array('action' => 'UserController@checkStep3', 'id'=>'signup')) }}
   {{ Form::label('skypeLabel', 'Skype (Zorunlu Değildir)') }} {{ Form::text('skypeText') }}
   {{ Form::submit('Tamamla !', array('class' => 'btn btn-primary btn-lg')) }}
