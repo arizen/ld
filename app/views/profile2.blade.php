5db97e1ce87e3ec978a34350b7a7e0cb9fd81184
estaril <ceironwars@gmail.com>
2014-06-12 21:47:39 +0300

    profile

--- app/views/profile2.blade.php
+++ app/views/profile2.blade.php
@@ -6,8 +6,29 @@
         <div class="row">
             <div class="col-md-12 col-sm-12">  
                 <div class="callout-box clearfix">  
-    <div class="sol"><img src="http://2.bp.blogspot.com/-QNSOqcH9ytU/TsnGM64faAI/AAAAAAAAAMk/6any0GmZFyU/s380/brights_icon_75x75.gif" alt="Üye Avatar"></div>
-                    <div class="sag"><div class="ssdivici" ><b style="color:#2772db; font-size:20px;">Güncel Ligim:</b><p style="color:#f7b53b;">ALTIN III</p></div> <div class="ssdivici" ><b style="color:#2772db; font-size:20px;">LD Puanım:</b><p style="color:red;">200 LDP</p></div><div class="ssdivici" ><b style="color:#2772db; font-size:20px;">Son Maçım:</b><p style="color:green;">Galibiyet</p></div></div>
+
+
+                    <div class="col-md-3 col-sm-6 col-xs-10"><b style="margin-left:20%; color:red; font-size:28px;">Kullanıcı</b><br /><br /><img src="http://avatar-user.s.aeriastatic.com/61386851/0327ddc4-84fe-4306-a367-80a8e947171e"></div> 
+                    
+
+                    <div class="col-md-3 col-sm-6 col-xs-10"><div class="col-md-12 col-sm-6 col-xs-4"><b style="color:#2772db; font-size:20px;">Güncel Ligim:</b><p style="color:#f7b53b;">ALTIN III</p></div><div class="col-md-12 col-sm-6 col-xs-4"><b style="color:#2772db; font-size:20px;"><img src="images/ldicon.png" width="64" height="64"> Puanım :</b><p style="color:red;">200 LDP</p></div><div class="col-md-12 col-sm-6 col-xs-4"><b style="color:#2772db; font-size:20px;">Son Maçım:</b><p style="color:green;">Galibiyet</p></div></div> 
+
+
+                    <div class="col-md-6 col-sm-6 col-xs-10"> <div class="content-box content-style3">
+
+        <div class="content-style3-icon icon-group"></div>
+        <div class="content-style3-title">
+            <h4 class="h4-body-title">Hakkımda</h4>
+        </div>
+        <div class="content-style3-text">
+            Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.
+        </div>
+
+</div>         </div> 
+                
+   
+
+
 </div>
             </div>
         </div>
