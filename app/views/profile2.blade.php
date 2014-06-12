5c0f2e96d08d66d0be1cc2061befe1c488f2f34b
estaril <ceironwars@gmail.com>
2014-06-12 20:17:22 +0300

    tasarımmasarımamk

--- app/views/profile2.blade.php
+++ app/views/profile2.blade.php
@@ -2,7 +2,15 @@
 @section('content')
 
 {{ HTML::ul($errors->all()) }}
-
+<div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12">  
+                <div class="callout-box clearfix">  
+    <div class="sol"><img src="http://2.bp.blogspot.com/-QNSOqcH9ytU/TsnGM64faAI/AAAAAAAAAMk/6any0GmZFyU/s380/brights_icon_75x75.gif" alt="Üye Avatar"></div>
+                    <div class="sag"><div class="ssdivici" ><b style="color:#2772db; font-size:20px;">Güncel Ligim:</b><p style="color:#f7b53b;">ALTIN III</p></div> <div class="ssdivici" ><b style="color:#2772db; font-size:20px;">LD Puanım:</b><p style="color:red;">200 LDP</p></div><div class="ssdivici" ><b style="color:#2772db; font-size:20px;">Son Maçım:</b><p style="color:green;">Galibiyet</p></div></div>
+</div>
+            </div>
+        </div>
 <div class="section-content section-tabs section-alter">
         <div class="tab-container">
     <div class="section-etabs-container">
@@ -11,10 +19,19 @@
                 <a href="#tabc1">Profil</a>
             </li>
             <li class="tab">
-                <a href="#tabc2">Yorumlar</a>
+                <a href="#tabc2">Son 10 Maç</a>
             </li>
             <li class="tab">
-                <a href="#tabc3">Üye Ayarları</a>
+                <a href="#tabc3">Yorumlar</a>
+            </li>
+            <li class="tab">
+                <a href="#tabc4">Duo İstekleri (0)</a>
+            </li>
+              <li class="tab">
+                <a href="#tabc5">Mesajlar (0)</a>
+            </li>
+              <li class="tab">
+                <a href="#tabc6">Kullanıcı Ayarları</a>
             </li>
         </ul>
     </div>
@@ -24,8 +41,10 @@
             
             <div id="tabc1">
                 <div class="row">
-        DENEME
-</div>            </div>
+ asdasdklsajdhnasdsalkşdasdklsjda sdlksjad aslkdjas daslkdasd aslkdjasdalskjdasdasldkjasdlkjn
+
+
+            </div></div>
             <div id="tabc2">
                <div class="row"></div>
         DENEME
@@ -33,9 +52,73 @@
             <div id="tabc3">
                 
                 <div class="row">
-        DENEME
+
+
+        <!-- Üye Ayar Başlangıç -->
+asdasdasd
+
+
+        <!-- Üye Ayar Bitiş -->
+
+
+
                 </div>
-            </div>            
+            </div>
+
+
+ <div id="tabc4">
+                
+                <div class="row">
+
+
+        <!-- Üye Ayar Başlangıç -->
+asdasdasd
+
+
+        <!-- Üye Ayar Bitiş -->
+
+
+
+                </div>
+            </div>
+
+
+            <div id="tabc5">
+                
+                <div class="row">
+
+
+        <!-- Üye Ayar Başlangıç -->
+asdasdasd123123123
+
+
+        <!-- Üye Ayar Bitiş -->
+
+
+
+                </div>
+            </div>
+
+
+            <div id="tabc6">
+                
+                <div class="row">
+
+
+        <!-- Üye Ayar Başlangıç -->
+asdasdasd'+'^324234234324'
+
+
+        <!-- Üye Ayar Bitiş -->
+
+
+
+                </div>
+            </div>
+
+
+
+
             
         </div>
     </div>
