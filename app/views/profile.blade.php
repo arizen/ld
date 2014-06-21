44ecaf0d16b31195f823371a7450be31a57ca541
estaril <ceironwars@gmail.com>
2014-06-22 01:42:37 +0300

    ligduzeltme

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -28,8 +28,8 @@
 <h3 class="profilei-title">Profil</h3>
 <div style="text-shadow: 1px 1px 1px #000; color:#D74536; font: normal 34px &quot;Trebuchet MS&quot;,Arial,sans-serif;font-weight: bold;margin-bottom: 6px; margin-left:1px; margin-top:6px;"><a href='http://leagueduo.com/profile/{{Session::get('user')->username}}'>{{Session::get('user')->username}}</a></div>
 <div style="position:static; top: 30px;"><img src="http://avatar-user.s.aeriastatic.com/61386851/0327ddc4-84fe-4306-a367-80a8e947171e" style="height: 200px;border-radius: 10px;border: 1px solid rgb(0, 0, 0);box-shadow: 0 0 10px #000;"></div>        
-  <div style="position:static; top: 30px;"><img src="http://elo-boost.net/images/tiers/challenger_1.png" style="height: 170px;"></div>
-   <div style="color: black; font: normal 26px &quot;Trebuchet MS&quot;,Arial,sans-serif;font-weight: bold;margin-top: -10px; margin-left:1px; margin-bottom:15px;">Diamond II</div>
+  <div style="position:static; top: 30px;"><img src="http://leagueduo.com/static/league/{{Session::get('summoner')->league}}.png" style="height: 170px;" alt="{{Session::get('summoner')->league}}"></div>
+   <div style="color: black; font: normal 26px &quot;Trebuchet MS&quot;,Arial,sans-serif;font-weight: bold;margin-top: -10px; margin-left:1px; margin-bottom:15px;">{{Session::get('summoner')->league}} {{Session::get('summoner')->division}}</div>
             
 <div id='profilemsol'>
 <ul>
