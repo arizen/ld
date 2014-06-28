d7cb5b5437189845046d5718f4a228be0e4b14e4
arizen <arizen47@gmail.com>
2014-06-28 22:43:08 +0300

    321

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -25,6 +25,9 @@
             <div class="col-md-12 col-sm-12">  
  <div class="row">
  <center> <div class="col-md-3"> <div class="profilei">
+  {{ Form::open(array('action' => 'DuoController@sendDuoRequest', 'id'=>'signup')) }}
+  {{ Form::submit('Duo Ekle', array('class' => 'btn btn-primary btn-lg')) }}
+{{ Form::close() }}
 <h3 class="profilei-title">Profil</h3>
 <div style="text-shadow: 1px 1px 1px #000; color:#D74536; font: normal 34px &quot;Trebuchet MS&quot;,Arial,sans-serif;font-weight: bold;margin-bottom: 6px; margin-left:1px; margin-top:6px;"><a href='http://leagueduo.com/profile/{{Session::get('user')->username}}'>{{Session::get('user')->username}}</a></div>
 <div style="position:static; top: 30px;"><img src="http://avatar-user.s.aeriastatic.com/61386851/0327ddc4-84fe-4306-a367-80a8e947171e" style="height: 200px;border-radius: 10px;border: 1px solid rgb(0, 0, 0);box-shadow: 0 0 10px #000;"></div>        
