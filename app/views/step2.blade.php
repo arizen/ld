5cd69eaa1988664d4cb280d0296c1d24d078ce57
estaril <ceironwars@gmail.com>
2014-07-07 23:22:16 +0300

    23462

--- app/views/step2.blade.php
+++ app/views/step2.blade.php
@@ -2,14 +2,47 @@
 @section('content')
 
 {{ HTML::ul($errors->all()) }}
+
+
+
 <center>
 {{ Form::open(array('action' => 'UserController@checkStep2', 'id'=>'signup')) }}
-    <img src="http://icons.iconarchive.com/icons/fazie69/league-of-legends/512/Acolyte-Lee-Sin-icon.png" height="128" width="128">  <input type="radio" name="avatar_id" value="avatar1">
-     <img src="http://icons.iconarchive.com/icons/fazie69/league-of-legends/512/Skarner-icon.png" height="128" width="128"> <input type="radio" name="avatar_id" value="avatar2">  
-    <img src="http://icons.iconarchive.com/icons/fazie69/league-of-legends/512/Acolyte-Lee-Sin-icon.png" height="128" width="128">  <input type="radio" name="avatar_id" value="avatar3">  <br />
-     <img src="http://icons.iconarchive.com/icons/fazie69/league-of-legends/512/Skarner-icon.png" height="128" width="128"> <input type="radio" name="avatar_id" value="avatar4"> 
-     <img src="http://icons.iconarchive.com/icons/fazie69/league-of-legends/512/Acolyte-Lee-Sin-icon.png" height="128" width="128"> <input type="radio" name="avatar_id" value="avatar5">  
-    <img src="http://icons.iconarchive.com/icons/fazie69/league-of-legends/512/Skarner-icon.png" height="128" width="128">  <input type="radio" name="avatar_id" value="avatar6"> <br /> 
+
+<table align="center" width="70%" border="1">
+  <tr>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/1.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/2.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/3.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/4.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/5.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/6.jpg"></td>
+  </tr>
+  <tr>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/7.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/8.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/9.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/10.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/11.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/12.jpg"></td>
+  </tr>
+  <tr>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/13.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/14.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/15.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/16.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/17.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/18.jpg"></td>
+  </tr>
+  <tr>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/19.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/20.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/21.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/22.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/23.jpg"></td>
+    <td style="padding-right: 5px;"><img src="http://leagueduo.com/static/avatar/24.jpg"></td>
+  </tr>
+</table>
+
       {{ Form::submit('İleri', array('class' => 'btn btn-primary btn-lg')) }}
 {{ Form::close() }}
 
